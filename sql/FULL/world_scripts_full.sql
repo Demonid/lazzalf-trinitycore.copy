c3241975e5630eaed8aac6c248dd08b33761f0b2
lazzalf <lazzalf@localhost>
2010-03-22 19:33:57 +0100

    Merge TC

--- sql/FULL/world_scripts_full.sql
+++ sql/FULL/world_scripts_full.sql
@@ -911,6 +911,7 @@ UPDATE `gameobject_template` SET `ScriptName`='containment_sphere' WHERE `entry`
 /* THE NEXUS, THE OCULUS */
 UPDATE `instance_template` SET `script`='instance_oculus' WHERE `map`=578;
 UPDATE `creature_template` SET `ScriptName`='boss_drakos' WHERE `entry`=27654;
+UPDATE `creature_template` SET `ScriptName`='npc_unstable_sphere' WHERE `entry`=28166;
 UPDATE `creature_template` SET `ScriptName`='npc_oculus_drakes' WHERE `entry` IN (27657,27658,27659);
 
 /* OBSIDIAN SANCTUM */
@@ -1353,6 +1354,12 @@ UPDATE `creature_template` SET `ScriptName`='boss_archavon' WHERE `entry`=31125;
 UPDATE `creature_template` SET `ScriptName`='mob_archavon_warder' WHERE `entry`=32353;
 UPDATE `creature_template` SET `ScriptName`='boss_emalon' WHERE `entry`=33993;
 UPDATE `creature_template` SET `ScriptName`='mob_tempest_minion' WHERE `entry`=33998;
+UPDATE `creature_template` SET `ScriptName`='boss_koralon' WHERE `entry`=35013;
+UPDATE `creature_template` SET `ScriptName`='mob_flame_warder' WHERE `entry`=35143;
+UPDATE `creature_template` SET `ScriptName`='boss_toravon' WHERE `entry`=38433;
+UPDATE `creature_template` SET `ScriptName`='mob_frost_warder' WHERE `entry`=38482;
+UPDATE `creature_template` SET `ScriptName`='mob_frozen_orb_stalker' WHERE `entry`=38461;
+UPDATE `creature_template` SET `ScriptName`='mob_frozen_orb' WHERE `entry`=38456;
 UPDATE `instance_template` SET `script`='instance_archavon' WHERE `map`=624;
 
 /* VIOLET HOLD */
