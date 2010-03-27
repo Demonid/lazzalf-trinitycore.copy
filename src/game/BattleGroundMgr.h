a93390f9bf1edb2440f1fc5d530b9ab2c56fd670
lazzalf <lazzalf@localhost>
2010-03-27 09:55:05 +0100

    Merge TC

--- src/game/BattleGroundMgr.h
+++ src/game/BattleGroundMgr.h
@@ -37,6 +37,7 @@ typedef UNORDERED_MAP<uint32, BattleGroundTypeId> BattleMastersMap;
 #define BATTLEGROUND_ARENA_POINT_DISTRIBUTION_DAY 86400     // seconds in a day
 #define BATTLEGROUND_ARENA_MOD_RESET_HOUR         3600      // seconds in an hour
 #define COUNT_OF_PLAYERS_TO_AVERAGE_WAIT_TIME 10
+#define WS_ARENA_DISTRIBUTION_TIME 20001                    // Custom worldstate
 
 struct QueueUpdateInfo
 {
