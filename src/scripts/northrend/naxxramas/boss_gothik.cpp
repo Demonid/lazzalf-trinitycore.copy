f73b85eb9430397bbaf3c7eca3f3439c9df04b7f
lazzalf <lazzalf@localhost>
2010-04-25 18:12:02 +0200

    Compile Fix :D

--- src/scripts/northrend/naxxramas/boss_gothik.cpp
+++ src/scripts/northrend/naxxramas/boss_gothik.cpp
@@ -137,7 +137,7 @@ struct NotOnSameSide : public std::unary_function<Unit *, bool>
     }
 };
 
-struct TRINITY_DLL_DECL boss_gothikAI : public BossAI
+struct boss_gothikAI : public BossAI
 {
     boss_gothikAI(Creature *c) : BossAI(c, BOSS_GOTHIK) {}
 
@@ -433,7 +433,7 @@ struct TRINITY_DLL_DECL boss_gothikAI : public BossAI
     }
 };
 
-struct TRINITY_DLL_DECL mob_gothik_minionAI : public CombatAI
+struct mob_gothik_minionAI : public CombatAI
 {
     mob_gothik_minionAI(Creature *c) : CombatAI(c)
     {
