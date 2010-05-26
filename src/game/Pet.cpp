ba2bd6b130bd162ab1b110a6e2e14fd5975f1b26
lazzalf <lazzalf@localhost>
2010-05-26 11:49:46 +0200

    Fix migliore per danno MirrorImage

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -1023,7 +1023,7 @@ bool Guardian::InitStatsForLevel(uint8 petlevel)
                 }
                 case 31216: // Mirror Image
                 {
-                    SetBonusDamage(int32(m_owner->SpellBaseDamageBonus(SPELL_SCHOOL_MASK_FROST) * 0.11f));
+                    SetBonusDamage(int32(m_owner->SpellBaseDamageBonus(SPELL_SCHOOL_MASK_FROST) * 0.33f));
                     SetDisplayId(m_owner->GetDisplayId());
                     if (!pInfo)
                     {
