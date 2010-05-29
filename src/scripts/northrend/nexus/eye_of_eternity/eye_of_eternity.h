bf25b3c736f51abc191e47f2daf2dffcd7d8e77a
shannonfano <shannonfano@localhost>
2010-05-29 12:56:21 +0200

    implementazione script malygos e fix reset

--- src/scripts/northrend/nexus/eye_of_eternity/eye_of_eternity.h
+++ src/scripts/northrend/nexus/eye_of_eternity/eye_of_eternity.h
@@ -8,8 +8,15 @@ enum
     TYPE_MALYGOS                 = 2,
     TYPE_OUTRO_CHECK             = 4,  // Because this encounter is really hard and outro takes about 90s and malygos dies at its end, I add some check against server crash
     TYPE_DESTROY_PLATFORM        = 5,
+    TYPE_VORTEX                  = 6,
+    TYPE_PLAYER_HOVER            = 7,
+    DATA_DROP_PLAYERS            = 8,
+    TYPE_SET_PLAYER_TO_CHECK     = 9,
+    TYPE_CHECK_PLAYER_FLYING     = 10,
     
     NPC_WYRMREST_SKYTALON        = 30161, // Dragon Vehicle in Third Phase
     NPC_MALYGOS                  = 28859,
+	GO_SPHERE					 = 193960
 };
+
 #endif
--- src/scripts/northrend/nexus/eye_of_eternity/instance_eye_of_eternity.cpp
+++ src/scripts/northrend/nexus/eye_of_eternity/instance_eye_of_eternity.cpp
@@ -8,7 +8,6 @@
 #define EXIT_Z 6987
 #define EXIT_Y 152
  
- 
 struct instance_eye_of_eternity : public ScriptedInstance
 {
     instance_eye_of_eternity(Map* pMap) : ScriptedInstance(pMap) {Initialize();}
@@ -18,11 +17,19 @@ struct instance_eye_of_eternity : public ScriptedInstance
     uint32 m_uiOutroCheck;
     uint32 m_uiMalygosPlatformData;
  
-    GameObject* m_uiMalygosPlatform;
-    GameObject* m_uiFocusingIris;
-    GameObject* m_uiExitPortal;
+    //GameObject* m_uiMalygosPlatform;
+    //GameObject* m_uiFocusingIris;
+    //GameObject* m_uiExitPortal;
+    uint64 m_uiMalygosPlatformGUID;
+    uint64 m_uiFocusingIrisGUID;
+    uint64 m_uiExitPortalGUID;
  
     uint64 m_uiMalygosGUID;
+    uint64 m_uiPlayerCheckGUID;
+
+    std::list<uint64> m_flyingPlayers;
+    
+    bool m_bVortex;
     
  
     void Initialize()
@@ -32,9 +39,11 @@ struct instance_eye_of_eternity : public ScriptedInstance
         m_uiMalygosGUID = 0;
         m_uiOutroCheck = 0; 
         m_uiMalygosPlatformData = 0;
-        m_uiMalygosPlatform = NULL;
-        m_uiFocusingIris = NULL;
-        m_uiExitPortal = NULL;
+        m_uiMalygosPlatformGUID = 0;
+        m_uiFocusingIrisGUID = 0;
+        m_uiExitPortalGUID = 0;
+        m_uiPlayerCheckGUID = 0;
+        m_bVortex = false;
     }
     
     void OnCreatureCreate(Creature* pCreature)
@@ -53,10 +62,10 @@ struct instance_eye_of_eternity : public ScriptedInstance
     {
         switch(pGo->GetEntry())
         {
-            case 193070: m_uiMalygosPlatform = pGo; break;
+            case 193070: m_uiMalygosPlatformGUID = pGo->GetGUID(); break;
             case 193958: //normal, hero 
-            case 193960: m_uiFocusingIris = pGo; break;
-            case 193908: m_uiExitPortal = pGo; break;
+            case GO_SPHERE: m_uiFocusingIrisGUID = pGo->GetGUID(); break;
+            case 193908: m_uiExitPortalGUID = pGo->GetGUID(); break;
             default:
                 break;
         }
@@ -78,10 +87,10 @@ struct instance_eye_of_eternity : public ScriptedInstance
             case TYPE_MALYGOS:
                 if(uiData == IN_PROGRESS)
                 {
-                  if(m_uiExitPortal)
-                      m_uiExitPortal->Delete();
-                  if(m_uiFocusingIris)
-                      m_uiFocusingIris->Delete();                
+                    if(GameObject* m_uiExitPortal = instance->GetGameObject(m_uiExitPortalGUID))
+                        m_uiExitPortal->Delete();
+                    if(GameObject* m_uiFocusingIris = instance->GetGameObject(m_uiFocusingIrisGUID))
+                        m_uiFocusingIris->Delete();
                 }
                 m_auiEncounter[0] = uiData;
                 break;
@@ -89,10 +98,31 @@ struct instance_eye_of_eternity : public ScriptedInstance
                 m_uiOutroCheck = uiData;
                 break;
             case TYPE_DESTROY_PLATFORM:
-                if(uiData == IN_PROGRESS && m_uiMalygosPlatform)
-                    m_uiMalygosPlatform->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_DESTROYED);
+                if(uiData == IN_PROGRESS)
+                {
+                    if(GameObject* m_uiMalygosPlatform = instance->GetGameObject(m_uiMalygosPlatformGUID))
+                        m_uiMalygosPlatform->SetFlag(GAMEOBJECT_FLAGS, GO_FLAG_DESTROYED);
+                }
+                else if(uiData == NOT_STARTED)
+                {
+                    if(GameObject* m_uiMalygosPlatform = instance->GetGameObject(m_uiMalygosPlatformGUID))
+                    {
+                        m_uiMalygosPlatform->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_DESTROYED);
+                        m_uiMalygosPlatform->Respawn();
+                    }
+                }
                 m_uiMalygosPlatformData = uiData;
-            break;
+                break;
+            case TYPE_VORTEX:
+                if(uiData)
+                    m_bVortex = true;
+                else
+                    m_bVortex = false;
+                break;
+            case TYPE_PLAYER_HOVER:
+                if(uiData == DATA_DROP_PLAYERS)
+                    dropAllPlayers();
+                break;
         }
     }
  
@@ -140,6 +170,10 @@ struct instance_eye_of_eternity : public ScriptedInstance
                 return m_uiOutroCheck;
             case TYPE_DESTROY_PLATFORM:
                 return m_uiMalygosPlatformData;
+            case TYPE_VORTEX:
+                return m_bVortex;
+            case TYPE_CHECK_PLAYER_FLYING:
+                return isFlying(m_uiPlayerCheckGUID);
         }
         return 0;
     }
@@ -150,10 +184,86 @@ struct instance_eye_of_eternity : public ScriptedInstance
         {
             case NPC_MALYGOS:
                 return m_uiMalygosGUID;
+			case GO_SPHERE:
+				return m_uiFocusingIrisGUID;
             default:
                 return 0;
         }
         return 0;
+    }
+
+    void SetData64(uint32 uiType, uint64 uiData)
+    {
+        switch(uiType)
+        {
+        case TYPE_PLAYER_HOVER:
+            if(!isFlying(uiData))
+            {
+                Unit* pUnit = Unit::GetUnit(*(instance->GetCreature(m_uiMalygosGUID)), uiData);
+                if(pUnit && pUnit->GetTypeId() == TYPEID_PLAYER)
+                {
+                    m_flyingPlayers.push_back(uiData);
+
+                    //Using packet workaround
+                    WorldPacket data(12);
+                    data.SetOpcode(SMSG_MOVE_SET_CAN_FLY);
+                    data.append(pUnit->GetPackGUID());
+                    data << uint32(0);
+                    pUnit->SendMessageToSet(&data, true);
+                }
+            }
+            break;
+
+        case TYPE_SET_PLAYER_TO_CHECK:
+            m_uiPlayerCheckGUID = uiData;
+            break;
+        }
+    }
+
+    bool isFlying(uint64 GUID)
+    {
+        if(m_flyingPlayers.empty())
+            return false;
+
+        for(std::list<uint64>::iterator itr = m_flyingPlayers.begin(); itr != m_flyingPlayers.end(); ++itr)
+        {
+            if(Player* pPlayer = (Player*)Unit::GetUnit(*(instance->GetCreature(m_uiMalygosGUID)), (*itr)))
+            {
+                if(pPlayer->GetGUID() == GUID)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    void dropAllPlayers()
+    {
+        for(std::list<uint64>::iterator itr = m_flyingPlayers.begin(); itr != m_flyingPlayers.end(); ++itr)
+        {
+            if(Unit* pUnit = Unit::GetUnit(*(instance->GetCreature(m_uiMalygosGUID)), (*itr)))
+            {
+                //Using packet workaround
+                WorldPacket data(12);
+                data.SetOpcode(SMSG_MOVE_UNSET_CAN_FLY);
+                data.append(pUnit->GetPackGUID());
+                data << uint32(0);
+                pUnit->SendMessageToSet(&data, true);
+            }
+        }
+        m_flyingPlayers.clear();
+    }
+
+    void OnPlayerEnter(Player* pPlayer)
+    {
+        if(GetData(TYPE_MALYGOS) == DONE)
+        {
+            Creature *pTemp = pPlayer->SummonCreature(NPC_WYRMREST_SKYTALON, pPlayer->GetPositionX(), pPlayer->GetPositionY(), pPlayer->GetPositionZ() - 5, 0);
+            if(pTemp)
+            {
+                pTemp->SetCreatorGUID(pPlayer->GetGUID());
+                pPlayer->EnterVehicle(pTemp, 0);
+            }
+        }
     }
 };
  
