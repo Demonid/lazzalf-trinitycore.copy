47d3779ee06a09f31e5e25b593d54c56d95e491b
Lazzalf <Lazzalf@localhost>
2010-06-07 18:03:51 +0200

    Fix Danni Force of Nature

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -948,9 +948,9 @@ bool Guardian::InitStatsForLevel(uint8 petlevel)
                 {
                     if (!pInfo)
                         SetCreateHealth(30 + 30*petlevel);
-                    int32 bonus_dmg = m_owner->SpellBaseDamageBonus(SPELL_SCHOOL_MASK_NATURE) * 0.15f;
-                    SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(petlevel * 7.5f - (petlevel / 2)+bonus_dmg));
-                    SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel * 7.5f + (petlevel / 2)+bonus_dmg));
+                    int32 bonus_dmg = m_owner->SpellBaseDamageBonus(SPELL_SCHOOL_MASK_NATURE) * 0.035f;
+                    SetBaseWeaponDamage(BASE_ATTACK, MINDAMAGE, float(petlevel * 4.0f - (petlevel / 2)+bonus_dmg));
+                    SetBaseWeaponDamage(BASE_ATTACK, MAXDAMAGE, float(petlevel * 4.0f + (petlevel / 2)+bonus_dmg));
                     break;
                 }
                 case 15352: //earth elemental 36213
