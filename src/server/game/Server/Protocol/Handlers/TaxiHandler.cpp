93293e07d1bc81d90a5184d7f4334ced2addf951
Lazzalf <Lazzalf@localhost>
2010-06-23 09:38:20 +0200

    Merge TC

--- src/server/game/Server/Protocol/Handlers/TaxiHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/TaxiHandler.cpp
@@ -297,7 +297,7 @@ void WorldSession::HandleMoveSplineDoneOpcode(WorldPacket& recv_data)
             FlightPathMovementGenerator* flight = (FlightPathMovementGenerator*)(GetPlayer()->GetMotionMaster()->top());
 
             flight->SetCurrentNodeAfterTeleport();
-            Path::PathNode const& node = flight->GetPath()[flight->GetCurrentNode()];
+            TaxiPathNodeEntry const& node = flight->GetPath()[flight->GetCurrentNode()];
             flight->SkipCurrentNode();
 
             GetPlayer()->TeleportTo(curDestNode->map_id,node.x,node.y,node.z,GetPlayer()->GetOrientation());
@@ -352,6 +352,8 @@ void WorldSession::HandleMoveSplineDoneOpcode(WorldPacket& recv_data)
             GetPlayer()->m_taxi.ClearTaxiDestinations();    // clear problematic path and next
         return;
     }
+    else
+        GetPlayer()->m_taxi.ClearTaxiDestinations();        // not destinations, clear source node
 
     GetPlayer()->CleanupAfterTaxiFlight();
     GetPlayer()->SetFallInformation(0, GetPlayer()->GetPositionZ());
