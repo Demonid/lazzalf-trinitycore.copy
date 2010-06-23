58447347f9c9064863815f957c74e7f071ecc0d9
Lazzalf <Lazzalf@localhost>
2010-06-23 14:32:41 +0200

    Rimossi parte dell'SQL precedente inutili

--- sql/custom/9279_ulduar_modelid.sql
+++ sql/custom/9279_ulduar_modelid.sql
@@ -1,12 +1,9 @@
 -- XT-002
 UPDATE `creature_model_info` SET `bounding_radius` = 0.465, `combat_reach` = 10 WHERE `modelid` = 28611;
-UPDATE `creature_template` SET `dmg_multiplier` = 70 WHERE `entry` = 33293;
 -- Assembly of Iron
 UPDATE `creature_model_info` SET `bounding_radius` = 0.45, `combat_reach` = 25 WHERE `modelid` = 28313;
 UPDATE `creature_model_info` SET `bounding_radius` = 0.45, `combat_reach` = 6 WHERE `modelid` = 28344;
 UPDATE `creature_template` SET `mechanic_immune_mask` = 617299547 WHERE `entry` = 32857;
-UPDATE `creature_template` SET `dmg_multiplier` = 60 WHERE `entry` = 32867;
-UPDATE `creature_template` SET `dmg_multiplier` = 45 WHERE `entry` = 32927;
 -- Kologarn
 UPDATE `creature_model_info` SET `bounding_radius` = 0.465, `combat_reach` = 15 WHERE `modelid` = 28638;
 UPDATE `creature_model_info` SET `bounding_radius` = 0.35, `combat_reach` = 15 WHERE `modelid` = 28821;
@@ -14,7 +11,6 @@ UPDATE `creature_model_info` SET `bounding_radius` = 0.35, `combat_reach` = 15 W
 UPDATE `creature_template` SET `baseattacktime` = 1800 WHERE `entry` = 32930;
 -- Auriaya
 UPDATE `creature_model_info` SET `bounding_radius` = 0.775, `combat_reach` = 5 WHERE `modelid` = 28651;
-UPDATE `creature_template` SET `dmg_multiplier` = 60 WHERE `entry` = 33515;
 -- Freya
 UPDATE `creature_template` SET `baseattacktime` = 1500 WHERE `entry` = 32906;
 -- Mimiron
