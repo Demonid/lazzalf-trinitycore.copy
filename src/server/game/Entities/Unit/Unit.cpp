3aca36beba83a35f22ad2b3f46ef5f16fcdc636f
lazzalf <lazzalf@localhost>
2010-07-11 22:47:25 +0200

    Merge TC

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -1830,9 +1830,8 @@ void Unit::CalcAbsorbResist(Unit *pVictim, SpellSchoolMask schoolMask, DamageEff
                     if (RemainingDamage < currentAbsorb)
                         currentAbsorb = RemainingDamage;
 
-                    // don't use up the aura if we don't absorb anything
-                    if (currentAbsorb > 0)
-                        (*i)->GetBase()->DropCharge();
+                    (*i)->SetAmount(0);     // guarantee removal
+                    existExpired = true;    // maybe hacky but not crashy
 
                     RemainingDamage -= currentAbsorb;
                     continue;
