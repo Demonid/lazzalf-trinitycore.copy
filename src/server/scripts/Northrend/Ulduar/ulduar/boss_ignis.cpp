aad498694cdb9ca5d19f3a769abc01bcdbede21d
Lazzalf <Lazzalf@localhost>
2010-08-31 12:21:58 +0200

    Fix compile 2

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_ignis.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_ignis.cpp
@@ -271,7 +271,7 @@ class boss_ignis : public CreatureScript
                     case EVENT_CONSTRUCT:
                         if (!construct_list.empty())
                         {
-                            ConstructVct::const_iterator itr = construct_list.begin();
+                            ConstructVct::iterator itr = construct_list.begin();
                             std::advance(itr, urand(0, construct_list.size()-1));
                             Creature* pTarget = Unit::GetCreature(*me, *itr);
                             if (pTarget)
