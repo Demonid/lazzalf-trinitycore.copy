d91faeb39badfee188fd87cafb7a2b8646de865d
Lazzalf <Lazzalf@localhost>
2010-09-02 18:47:48 +0200

    Tentativo di fix casse di Thorim

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_thorim.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_thorim.cpp
@@ -291,11 +291,11 @@ class boss_thorim : public CreatureScript
                 if (HardMode)
                 {
                     pInstance->DoCompleteAchievement(ACHIEVEMENT_LOSE_ILLUSION);
-                    me->SummonGameObject(RAID_MODE(CACHE_OF_STORMS_HARDMODE_10, CACHE_OF_STORMS_HARDMODE_25), 2134.58f, -286.908f, 419.495f, 1.55988f, 0, 0, 0.7f, 0.7f, 604800);
+                    me->SummonGameObject(RAID_MODE(CACHE_OF_STORMS_HARDMODE_10, CACHE_OF_STORMS_HARDMODE_25), 2134.58f, -286.908f, 419.495f, 1.55988f, 0, 0, 0.7f, 0.7f, 0); //604800
                 }
                 else
                 {
-                    me->SummonGameObject(RAID_MODE(CACHE_OF_STORMS_10, CACHE_OF_STORMS_25), 2134.58f, -286.908f, 419.495f, 1.55988f, 0, 0, 0.7f, 0.7f, 604800);
+                    me->SummonGameObject(RAID_MODE(CACHE_OF_STORMS_10, CACHE_OF_STORMS_25), 2134.58f, -286.908f, 419.495f, 1.55988f, 0, 0, 0.7f, 0.7f, 0); //604800
                 }
             }
         }
