b36de7dfc6cfa390ffe78b5d889fb981adb07a61
Lazzalf <Lazzalf@localhost>
2010-09-09 01:15:36 +0200

    Merge

---
+++ cmake/platform/settings.win32.cmake
@@ -0,0 +1,28 @@
+# Platform-specfic options
+option(EXTERNAL_MYSQL "Use your own installed MySQL instead of the internal one" 0)
+
+# Package overloads
+set(ACE_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/acelite)
+set(ACE_LIBRARY "ace")
+set(BZIP2_LIBRARIES "bzip2")
+set(ZLIB_LIBRARIES "zlib")
+
+if( NOT EXTERNAL_MYSQL )
+  set(MYSQL_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/dep/mysqllite/include)
+  set(MYSQL_LIBRARY "libmysql")
+  set( MYSQL_FOUND 1 )
+  message(STATUS "Using internal MySQL.")
+endif()
+
+# check the CMake preload parameters (commented out by default)
+
+# overload CMAKE_INSTALL_PREFIX if not being set properly
+#if( WIN32 )
+#  if( NOT CYGWIN )
+#    if( NOT CMAKE_INSTALL_PREFIX )
+#      set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/bin")
+#    endif()
+#  endif()
+#endif()
+
+include(${CMAKE_SOURCE_DIR}/cmake/compiler/msvc/settings.cmake)
