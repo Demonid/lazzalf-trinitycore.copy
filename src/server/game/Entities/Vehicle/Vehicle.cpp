14d349811139e39bb6e9b731e60c3f02dfbddf2e
Lazzalf <Lazzalf@localhost>
2010-09-10 09:08:26 +0200

    Fixato il bug dell'Expose Heart di XT-002 che a volte "impazziva" by BloodyCore

--- src/server/game/Entities/Vehicle/Vehicle.cpp
+++ src/server/game/Entities/Vehicle/Vehicle.cpp
@@ -320,9 +320,9 @@ bool Vehicle::AddPassenger(Unit *unit, int8 seatId)
     {
         switch (GetVehicleInfo()->m_ID)
         {
-            case 342: //Ignis
-            case 353: //case 335: //XT-002
-            case 380: //Kologarn's Right Arm
+            case 342: // Ignis
+            case 353: // XT-002
+            case 380: // Kologarn's Right Arm
                 break;
             default: 
                 unit->addUnitState(UNIT_STAT_ONVEHICLE); 
