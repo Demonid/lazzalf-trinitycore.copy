14c6714b0b48b6c315ba6fdf8ef5ede72d43e862
Lazzalf <Lazzalf@localhost>
2010-09-14 00:43:34 +0200

    typo

--- src/server/scripts/Northrend/VioletHold/instance_violet_hold.cpp
+++ src/server/scripts/Northrend/VioletHold/instance_violet_hold.cpp
@@ -585,7 +585,7 @@ public:
                         do
                         {
                             uiSecondBoss = urand(1,6);
-                        } while ((uiSecondBoss == uiFirstBoss) || (uiSecondBoss == 3));
+                        } while ((uiSecondBoss == uiFirstBoss) || (uiSecondBoss == 3))
                     if (Creature *pSinclari = instance->GetCreature(uiSinclari))
                     {
                         if(Creature *pPortal = pSinclari->SummonCreature(CREATURE_TELEPORTATION_PORTAL, MiddleRoomPortalSaboLocation, TEMPSUMMON_CORPSE_DESPAWN))
