fdc9a9c51cbe0bfd81f9c80d193dc430fbd646bf
Lazzalf <Lazzalf@localhost>
2010-09-19 17:42:01 +0200

    Disattivato temporanemanete l'hand_of_death

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_hodir.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_hodir.cpp
@@ -411,8 +411,7 @@ class boss_hodir : public CreatureScript
                     else
                     {
                         if (GetClosestCreatureWithEntry(pTarget, NPC_ICICLE_TARGET, 5.0f))
-                            continue;
-                            
+                            continue;                            
                         else if (Creature *pIceBlock = me->SummonCreature(NPC_FLASH_FREEZE, pTarget->GetPositionX(), pTarget->GetPositionY(), pTarget->GetPositionZ(), 0, TEMPSUMMON_TIMED_DESPAWN, 105000))
                         { 
                            pIceBlock->CastSpell(pTarget, SPELL_BLOCK_OF_ICE, true);
