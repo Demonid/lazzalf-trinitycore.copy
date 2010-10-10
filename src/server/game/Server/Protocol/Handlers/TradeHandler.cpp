9c0c162073ff55e6d2a64e8558f420feb5235278
Lazzalf <Lazzalf@localhost>
2010-10-10 10:35:18 +0200

    Merge TC

--- src/server/game/Server/Protocol/Handlers/TradeHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/TradeHandler.cpp
@@ -159,9 +159,10 @@ void WorldSession::moveItems(Item* myItems[], Item* hisItems[])
                         trader->GetName(), trader->GetSession()->GetAccountId());
                 }
 
-                // store
+                // adjust time (depends on /played)
                 if (myItems[i]->HasFlag(ITEM_FIELD_FLAGS, ITEM_FLAG_BOP_TRADEABLE))
                     myItems[i]->SetUInt32Value(ITEM_FIELD_CREATE_PLAYED_TIME, trader->GetTotalPlayedTime()-(_player->GetTotalPlayedTime()-myItems[i]->GetUInt32Value(ITEM_FIELD_CREATE_PLAYED_TIME)));
+                // store
                 trader->MoveItemToInventory(traderDst, myItems[i], true, true);
             }
             if (hisItems[i])
@@ -176,9 +177,10 @@ void WorldSession::moveItems(Item* myItems[], Item* hisItems[])
                         _player->GetName(), _player->GetSession()->GetAccountId());
                 }
 
-                // store
+                // adjust time (depends on /played)
                 if (hisItems[i]->HasFlag(ITEM_FIELD_FLAGS, ITEM_FLAG_BOP_TRADEABLE))
                     hisItems[i]->SetUInt32Value(ITEM_FIELD_CREATE_PLAYED_TIME, _player->GetTotalPlayedTime()-(trader->GetTotalPlayedTime()-hisItems[i]->GetUInt32Value(ITEM_FIELD_CREATE_PLAYED_TIME)));
+                // store
                 _player->MoveItemToInventory(playerDst, hisItems[i], true, true);
             }
         }
