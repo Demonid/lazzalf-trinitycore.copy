3cbfaed21560e6c99134b39938e7f7e497af5631
Lazzalf <Lazzalf@localhost>
2010-10-18 17:40:26 +0200

    Rimesso codice rimosso da Trinity

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -14496,6 +14496,11 @@ void Unit::StopMoving()
     //Relocate(GetPositionX(), GetPositionY(),GetPositionZ());
 
     SendMonsterStop();
+
+    // update position and orientation;
+    WorldPacket data;
+    BuildHeartBeatMsg(&data);
+    SendMessageToSet(&data,false);
 }
 
 void Unit::SendMovementFlagUpdate()
