27b03c3410922ed444889603dce5a32b38a459c6
Lazzalf <Lazzalf@localhost>
2010-10-26 11:25:31 +0200

    Reso Onyxia immune al Death Grip e al KnockBack

--- src/server/scripts/Kalimdor/OnyxiasLair/boss_onyxia.cpp
+++ src/server/scripts/Kalimdor/OnyxiasLair/boss_onyxia.cpp
@@ -114,7 +114,9 @@ public:
     {
         boss_onyxiaAI(Creature* pCreature) : ScriptedAI(pCreature), Summons(me)
         {
-            m_pInstance = pCreature->GetInstanceScript();
+            m_pInstance = pCreature->GetInstanceScript();            
+            me->ApplySpellImmune(0, IMMUNITY_EFFECT, SPELL_EFFECT_KNOCK_BACK, true);
+            me->ApplySpellImmune(0, IMMUNITY_ID, 49560, true); // Death Grip jump effect
             Reset();
         }
 
