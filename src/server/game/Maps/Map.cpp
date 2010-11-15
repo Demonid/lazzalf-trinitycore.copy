5026041fb88b11d9ddff7ce61234e13285e7028f
Lazzalf <Lazzalf@localhost>
2010-11-15 15:49:58 +0100

    Fix typo

--- src/server/game/Maps/Map.cpp
+++ src/server/game/Maps/Map.cpp
@@ -548,9 +548,7 @@ void Map::Update(const uint32 &t_diff)
         i_scriptLock = false;
     }
 
-    //m_MoveAllCreaturesInMoveListMtx.acquire();
     MoveAllCreaturesInMoveList();
-    //m_MoveAllCreaturesInMoveListMtx.release();
 
     if (!m_mapRefManager.isEmpty() || !m_activeNonPlayers.empty())
         ProcessRelocationNotifies(t_diff);
