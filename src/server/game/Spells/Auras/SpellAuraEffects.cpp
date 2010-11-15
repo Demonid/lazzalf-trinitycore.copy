0006eb6efc7cb605297a5702c7751cac6ced89ff
Lazzalf <Lazzalf@localhost>
2010-11-15 11:06:11 +0100

    Cleanup

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -4066,20 +4066,7 @@ void AuraEffect::HandleModThreat(AuraApplication const * aurApp, uint8 mode, boo
     if (target->GetTypeId() == TYPEID_PLAYER)
         for (int8 x=0; x < MAX_SPELL_SCHOOL; x++)
             if (GetMiscValue() & int32(1<<x))
-            {
-                /*float m_amount = 0;
-                switch(GetId())
-                {
-                    case 21178:
-                        m_amount = 107.35f; //HackFix per Dudu BearForm
-                        break;
-                    default:
-                        m_amount = (float)GetAmount();
-                        break;
-                }
-                ApplyPercentModFloatVar(target->m_threatModifier[x], m_amount, apply);*/
                 ApplyPercentModFloatVar(target->m_threatModifier[x], (float)GetAmount(), apply);
-            }
 }
 
 void AuraEffect::HandleAuraModTotalThreat(AuraApplication const * aurApp, uint8 mode, bool apply) const
