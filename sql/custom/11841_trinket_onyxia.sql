e112c2bb834baf0695f4139d9c9899c1ad3ba4d4
Lazzalf <Lazzalf@localhost>
2010-11-16 10:02:38 +0100

    Sql per trinket onyxia

---
+++ sql/custom/11841_trinket_onyxia.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` where `entry` in (69739,69755);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) values('69739','0','0','0','0','0','81920','3','0','30','45');
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) values('69755','0','0','0','0','0','81920','3','0','30','45');
