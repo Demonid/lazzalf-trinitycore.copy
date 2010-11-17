b6272760a1907a39848dcdd1f32ccdb00c3e3964
Lazzalf <Lazzalf@localhost>
2010-11-17 19:09:54 +0100

    Aggiunto debug per toc

--- src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_faction_champions.cpp
+++ src/server/scripts/Northrend/CrusadersColiseum/TrialOfTheCrusader/boss_faction_champions.cpp
@@ -196,6 +196,7 @@ public:
                     {
                         case FAIL:
                             m_uiChampionsFailed++;
+                            sLog.outCheat("ToC Id: %u Debug info m_uiChampionsFailed: %u, summon size: %u, m_uiChampionsKilled: %u",me->GetInstanceId(),m_uiChampionsFailed, Summons.size(), m_uiChampionsKilled);
                             if (m_uiChampionsFailed + m_uiChampionsKilled >= Summons.size())
                             {
                                 m_pInstance->SetData(TYPE_CRUSADERS, FAIL);
@@ -216,6 +217,7 @@ public:
                             break;
                         case DONE:
                             m_uiChampionsKilled++;
+                            sLog.outCheat("ToC Id: %u Debug info m_uiChampionsKilled: %u, summon size: %u",me->GetInstanceId(),m_uiChampionsKilled, Summons.size());
                             if (m_uiChampionsKilled == 1)
                                 m_pInstance->SetData(TYPE_CRUSADERS, SPECIAL);
                             else if (m_uiChampionsKilled >= Summons.size())
@@ -261,6 +263,7 @@ struct boss_faction_championsAI : public ScriptedAI
         if (m_pInstance)
             if (Creature* pChampionController = Unit::GetCreature((*me),m_pInstance->GetData64(NPC_CHAMPIONS_CONTROLLER)))
                 pChampionController->AI()->SetData(2, FAIL);
+        sLog.outCheat("ToC Id: %u Debug info Reset entry: %u", me->GetInstanceId(), me->GetEntry());                         
         me->ForcedDespawn();
     }
 
@@ -317,6 +320,7 @@ struct boss_faction_championsAI : public ScriptedAI
             if (m_pInstance)
                 if (Creature* pChampionController = Unit::GetCreature((*me),m_pInstance->GetData64(NPC_CHAMPIONS_CONTROLLER)))
                     pChampionController->AI()->SetData(2, DONE);
+        sLog.outCheat("ToC Id: %u Debug info Die entry: %u", me->GetInstanceId(), me->GetEntry()); 
     }
 
     void EnterCombat(Unit* /*who*/)
