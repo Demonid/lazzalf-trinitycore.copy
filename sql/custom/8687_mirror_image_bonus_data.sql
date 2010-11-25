d80b7bda9a38341bbbfb2ed41d9aeabf9a2b61b3
Lazzalf <Lazzalf@localhost>
2010-11-25 16:42:33 +0100

    Fix cura Paladin T8 Holy 2P Bonus

---
+++ sql/custom/8687_mirror_image_bonus_data.sql
@@ -0,0 +1,4 @@
+DELETE FROM spell_bonus_data WHERE entry IN (59638, 59637);
+INSERT INTO spell_bonus_data VALUES
+(59638, 0.3, -1, -1, -1, 'Mage - Mirror Image Frostbolt'),
+(59637, 0.15, -1, -1, -1, 'Mage - Mirror Image Fire blast');
