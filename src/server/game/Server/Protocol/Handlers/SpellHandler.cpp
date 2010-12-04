705f1df4076a5c3f2baaeda11ac9325f05b7d96b
Lazzalf <Lazzalf@localhost>
2010-12-04 09:22:19 +0100

    Crash Fix

--- src/server/game/Server/Protocol/Handlers/SpellHandler.cpp
+++ src/server/game/Server/Protocol/Handlers/SpellHandler.cpp
@@ -320,7 +320,8 @@ void WorldSession::HandleGameobjectReportUse(WorldPacket& recvPacket)
     if (!go->IsWithinDistInMap(_player,INTERACTION_DISTANCE))
         return;
 
-    go->AI()->GossipHello(_player);
+    if (go->AI())
+        go->AI()->GossipHello(_player);
 
     _player->GetAchievementMgr().UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_USE_GAMEOBJECT, go->GetEntry());
 }
