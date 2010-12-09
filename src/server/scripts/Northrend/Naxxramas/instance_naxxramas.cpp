15b8e5ef8b901471c4e11ec54e10224bbdb0bc2e
Lazzalf <Lazzalf@localhost>
2010-12-09 22:08:51 +0100

    Small codestyle

--- src/server/scripts/Northrend/Naxxramas/instance_naxxramas.cpp
+++ src/server/scripts/Northrend/Naxxramas/instance_naxxramas.cpp
@@ -196,13 +196,29 @@ public:
                     GothikGateGUID = go->GetGUID();
                     go->SetGoState(gothikDoorState);
                     break;
-                case GO_HORSEMEN_CHEST: HorsemenChestGUID = go->GetGUID(); break;
-                case GO_HORSEMEN_CHEST_HERO: HorsemenChestGUID = go->GetGUID(); break;
-                case GO_KELTHUZAD_PORTAL01: uiPortals[0] = go->GetGUID(); break;
-                case GO_KELTHUZAD_PORTAL02: uiPortals[1] = go->GetGUID(); break;
-                case GO_KELTHUZAD_PORTAL03: uiPortals[2] = go->GetGUID(); break;
-                case GO_KELTHUZAD_PORTAL04: uiPortals[3] = go->GetGUID(); break;
-                case GO_KELTHUZAD_TRIGGER: uiKelthuzadTrigger = go->GetGUID(); break;
+                case GO_HORSEMEN_CHEST: 
+                    HorsemenChestGUID = go->GetGUID(); 
+                    break;
+                case GO_HORSEMEN_CHEST_HERO: 
+                    HorsemenChestGUID = go->GetGUID(); 
+                    break;
+                case GO_KELTHUZAD_PORTAL01: 
+                    uiPortals[0] = go->GetGUID(); 
+                    break;
+                case GO_KELTHUZAD_PORTAL02: 
+                    uiPortals[1] = go->GetGUID(); 
+                    break;
+                case GO_KELTHUZAD_PORTAL03: 
+                    uiPortals[2] = go->GetGUID(); 
+                    break;
+                case GO_KELTHUZAD_PORTAL04: 
+                    uiPortals[3] = go->GetGUID(); 
+                    break;
+                case GO_KELTHUZAD_TRIGGER: 
+                    uiKelthuzadTrigger = go->GetGUID(); 
+                    break;
+                default:
+                    break;
             }
 
             AddDoor(go, true);
