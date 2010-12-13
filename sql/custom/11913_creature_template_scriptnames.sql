4ce904861c132a2f4e2057a6806550f9d771f42f
MarcoNike <lightshard@hotmail.it>
2010-12-13 13:46:58 +0100

    Altri fix: - Fix credito quest Escape from Durnholde - Le piattaforme a Ulduar non riparano se il veicolo ? full vita (Rimessa la condizione che era stata tolta in un fix precedente) - Despawn degli elementali di fuoco da Sartharion su Reset e JustDied

---
+++ sql/custom/11913_creature_template_scriptnames.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `ScriptName`='mob_unstoppable_abomination' WHERE `entry`=16428;
+UPDATE `creature_template` SET `ScriptName`='mob_void_sentry' WHERE `entry`=29364;
+UPDATE `creature_template` SET `ScriptName`='mob_steelforged_defender' WHERE `entry`='33236';
