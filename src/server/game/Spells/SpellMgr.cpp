2570aa3274aa5b8d658b134105773d7f7995a01d
Lazzalf <Lazzalf@localhost>
2010-12-15 14:11:25 +0100

    Fix some DiminishingReturn patch by Dr.tenma

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -2837,9 +2837,16 @@ void SpellMgr::LoadSkillLineAbilityMap()
 
 DiminishingGroup GetDiminishingReturnsGroupForSpell(SpellEntry const* spellproto, bool triggered)
 {
+    if (IsPositiveSpell(spellproto->Id))
+        return DIMINISHING_NONE;
+
     // Explicit Diminishing Groups
     switch (spellproto->SpellFamilyName)
     {
+        // Event spells
+        case SPELLFAMILY_UNK1:
+            return DIMINISHING_NONE;
+            break;
         case SPELLFAMILY_GENERIC:
             // some generic arena related spells have by some strange reason MECHANIC_TURN
             if  (spellproto->Mechanic == MECHANIC_TURN)
