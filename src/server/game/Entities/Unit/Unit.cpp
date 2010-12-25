a4821569f1b1af766deb0b2869114ce6a2e827a8
Lazzalf <Lazzalf@localhost>
2010-12-25 18:50:04 +0100

    Fix overpower damage

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11848,26 +11848,24 @@ void Unit::MeleeDamageBonus(Unit *pVictim, uint32 *pdamage, WeaponAttackType att
     AuraEffectList const &mModDamagePercentDone = GetAuraEffectsByType(SPELL_AURA_MOD_DAMAGE_PERCENT_DONE);
     for (AuraEffectList::const_iterator i = mModDamagePercentDone.begin(); i != mModDamagePercentDone.end(); ++i)
         if (spellProto)
-        {
-            AuraEffectList const &mModDamagePercentDone = GetAuraEffectsByType(SPELL_AURA_MOD_DAMAGE_PERCENT_DONE);
-            for (AuraEffectList::const_iterator i = mModDamagePercentDone.begin(); i != mModDamagePercentDone.end(); ++i)
-                if ((*i)->GetMiscValue() & GetSpellSchoolMask(spellProto))
-                {
-                    if ((*i)->GetSpellProto()->EquippedItemClass == -1)
-                    {
-                        AddPctN(DoneTotalMod, (*i)->GetAmount());
-                    }
-                    else if (!((*i)->GetSpellProto()->AttributesEx5 & SPELL_ATTR5_SPECIAL_ITEM_CLASS_CHECK))
-                    {
-                        if ((*i)->GetSpellProto()->EquippedItemClass & spellProto->EquippedItemClass)
-                            if (((*i)->GetSpellProto()->EquippedItemSubClassMask == 0) ||
-                                ((*i)->GetSpellProto()->EquippedItemSubClassMask & spellProto->EquippedItemSubClassMask))
-                                AddPctN(DoneTotalMod, (*i)->GetAmount());
-                    }
-                    else if (ToPlayer() && ToPlayer()->HasItemFitToSpellRequirements((*i)->GetSpellProto()))
-                        AddPctN(DoneTotalMod, (*i)->GetAmount());
-                }
-        }
+		{
+			if ((*i)->GetMiscValue() & GetSpellSchoolMask(spellProto))
+			{
+				if ((*i)->GetSpellProto()->EquippedItemClass == -1)
+				{
+					AddPctN(DoneTotalMod, (*i)->GetAmount());
+				}
+				else if (!((*i)->GetSpellProto()->AttributesEx5 & SPELL_ATTR5_SPECIAL_ITEM_CLASS_CHECK))
+				{
+					if ((*i)->GetSpellProto()->EquippedItemClass & spellProto->EquippedItemClass)
+						if (((*i)->GetSpellProto()->EquippedItemSubClassMask == 0) ||
+							((*i)->GetSpellProto()->EquippedItemSubClassMask & spellProto->EquippedItemSubClassMask))
+								AddPctN(DoneTotalMod, (*i)->GetAmount());
+				}
+				else if (ToPlayer() && ToPlayer()->HasItemFitToSpellRequirements((*i)->GetSpellProto()))
+						AddPctN(DoneTotalMod, (*i)->GetAmount());
+			}
+		}
         else if (ToPlayer())
         {
             if (!((*i)->GetSpellProto()->AttributesEx5 & SPELL_ATTR5_SPECIAL_ITEM_CLASS_CHECK))
