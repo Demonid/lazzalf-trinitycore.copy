5e4636f4c50c1a38cae26ee39765a73bf075b7bf
Lazzalf <Lazzalf@localhost>
2010-12-29 00:09:29 +0100

    Backed out changeset: ea4c97ef3e2f

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8129,8 +8129,8 @@ bool Unit::HandleAuraProc(Unit * pVictim, uint32 damage, Aura * triggeredByAura,
                     if (pVictim->HasAura(53601))
                     {
                         int32 bp0 = CalculatePctN(int32(damage / 12), SpellMgr::CalculateSpellEffectAmount(dummySpell, 2));
-                        //if (HasAura(67191)) // Item - Paladin Holy T9 4P
-                        //    bp0 *= 2;
+                        if (HasAura(67191)) // Item - Paladin Holy T9 4P
+                            bp0 *= 2;
                         CastCustomSpell(pVictim, 66922, &bp0, NULL, NULL, true);
                         return true;
                     }
